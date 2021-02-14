bc773cd51d6417ab18e75997b82209b25593da06
Maczi01 <mateusz.w.twardy@gmail.com>
2021-02-14 22:38:03 +0100

    add Rest Controller

---
+++ src/main/java/com/example/attentioncontolbackend/NumbersApi.java
@@ -0,0 +1,31 @@
+package com.example.attentioncontolbackend;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class NumbersApi {
+
+    private List<Integer> array;
+
+    public NumbersApi() {
+        this.array = new ArrayList<>();
+        array.add(1);
+        array.add(2);
+        array.add(3);
+        array.add(4);
+    }
+
+    @GetMapping()
+    public List<Integer> getNumbers(){
+        return array;
+    }
+    @PostMapping()
+    public void giveNumbers(Object p){
+        System.out.println(p.toString());
+    }
+}
