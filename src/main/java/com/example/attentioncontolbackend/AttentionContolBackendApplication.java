5996be29f993c0d658cb062082a9bf95c195e760
Maczi01 <mateusz.w.twardy@gmail.com>
2021-02-14 21:13:06 +0100

    Initial commit

---
+++ src/main/java/com/example/attentioncontolbackend/AttentionContolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.attentioncontolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttentionContolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AttentionContolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/attentioncontolbackend/AttentionContolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.attentioncontolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttentionContolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
