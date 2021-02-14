8fb7d500fb36432a41c94159df3e85e30a6f58e0
Maczi01 <mateusz.w.twardy@gmail.com>
2021-02-14 23:58:16 +0100

    Add method initGame

---
+++ src/main/java/com/example/attentioncontolbackend/logic/Game.java
@@ -0,0 +1,41 @@
+package com.example.attentioncontolbackend.logic;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Game {
+
+    private Random rand = new Random();
+    private int[] board = new int[99];
+    private int counter;
+    private boolean isPlaying;
+    private int result;
+
+    public int[] generateNumbers() {
+        int[] array = new int[99];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i;
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
+    }
+
+    public int[] generateMixedArray() {
+        int[] array = generateNumbers();
+        for (int i = 0; i < array.length; i++) {
+            int randomIndexToSwap = rand.nextInt(array.length);
+            int temp = array[randomIndexToSwap];
+            array[randomIndexToSwap] = array[i];
+            array[i] = temp;
+        }
+        System.out.println(Arrays.toString(array));
+        return array;
+    }
+
+    public void initGame(){
+        board = generateMixedArray();
+        counter = 0;
+        result = 0;
+        isPlaying = true;
+    }
+}
---
+++ src/main/java/com/example/attentioncontolbackend/service/NumbersService.java
@@ -0,0 +1,14 @@
+package com.example.attentioncontolbackend.service;
+
+import com.example.attentioncontolbackend.logic.Game;
+
+public class NumbersService {
+
+    private final Game game;
+
+    public NumbersService(Game game) {
+        this.game = game;
+    }
+
+
+}
